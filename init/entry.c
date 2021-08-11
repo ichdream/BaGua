4edb07e79bc4c97dbe197bba3751a3ec0a4043f5
ichdream <chensheng0802@outlook.com>
2021-08-11 22:22:02 +0800

    添加中断注册，中断请求服务，添加定时器中断功能

--- init/entry.c
+++ init/entry.c
@@ -1,4 +1,6 @@
 #include "gdt.h"
+#include "idt.h"
+#include "timer.h"
 #include "console.h"
 #include "debug.h"
 
@@ -7,13 +9,24 @@ int kern_entry()
     console_clear();
     console_write_color("GDT table init...\n", rc_black, rc_green);
     init_gdt();
+    console_write_color("IDT table init...\n", rc_black, rc_green);
+    init_idt();
 
-    console_write_color("Hello, this is BaGua OS kernel， V0.01!\n", rc_black, rc_green);
+    // asm volatile ("int $0x3");
+	// asm volatile ("int $0x4");
+
+    console_write_color("Hello, this is BaGua OS kernel， V0.02!\n", rc_black, rc_green);
     printk("******* Kernel-level printk function utility had supported. *******\n");
     print_cur_status();
 
-    printk("kerbel debug:\n");
-    panic("test");
+    console_write_color("timer interrupt...!\n", rc_black, rc_green);
+    init_timer(200);
+
+	// 开启中断
+	asm volatile ("sti");
+
+    // printk("kerbel debug:\n");
+    // panic("test");
     
     return 0;
 }

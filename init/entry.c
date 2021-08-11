9ddc92ebdc32d1270b85d1ef5ba2baca96685acf
ichdream <chensheng0802@outlook.com>
2021-08-11 16:37:02 +0800

    修改entry函数，可以看见gbt初始化过程...

--- init/entry.c
+++ init/entry.c
@@ -1,12 +1,19 @@
+#include "gdt.h"
 #include "console.h"
-# include "debug.h"
+#include "debug.h"
 
 int kern_entry()
 {
     console_clear();
+    console_write_color("GDT table init...\n", rc_black, rc_green);
+    init_gdt();
 
-    console_write_color("Hello, this is BaGua OS kernel!\n", rc_black, rc_green);
+    console_write_color("Hello, this is BaGua OS kernel， V0.01!\n", rc_black, rc_green);
     printk("******* Kernel-level printk function utility had supported. *******\n");
+    print_cur_status();
+
+    printk("kerbel debug:\n");
     panic("test");
+    
     return 0;
 }
